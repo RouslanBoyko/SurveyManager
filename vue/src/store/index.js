4d0e54baa1be1a0530e4e27594505e79959737ac
Rouslan <Rouslanboyko.webdeveloper@gmail.com>
2022-01-31 14:23:20 +0100

    VueJs & tailwind setup

---
+++ vue/src/store/index.js
@@ -0,0 +1,16 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+  state: {
+    user: {
+      data: {name: 'rouslan'},
+      token: null
+    }
+  },
+  getters: {},
+  actions: {},
+  mutations: {},
+  modules: {},
+})
+
+export default store;
