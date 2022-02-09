411d9b69d3f6741ca044b8c8a39a12f12224170d
Rouslan <Rouslanboyko.webdeveloper@gmail.com>
2022-02-09 10:22:40 +0100

    Logout backend

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -64,4 +64,17 @@ class AuthController extends Controller
             'token' => $token
         ]);
     }
+
+    public function logout()
+    {
+        /** @var User $user */
+        $user = Auth::user();
+
+        // Revoke the token that was used to authenticate the current request...
+        $user->currentAccessToken()->delete();
+
+        return response([
+           'success' => true
+        ]);
+    }
 }
